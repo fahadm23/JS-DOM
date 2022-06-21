461cb52dc593f0c292a00114b9c26ac2a23a9918
Fahad Mohammad <fahad.mohammad06@gmail.com>
2022-06-21 03:36:58 -0400

    Updated dom2.js

--- dom2.js
+++ dom2.js
@@ -1,39 +1,39 @@
 // TRAVERSING THE DOM //
 var itemList = document.querySelector('#items');
 //parentNode
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentNode.parentNode);
-// console.log(itemList.parentNode.parentNode.parentNode);
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode);
+console.log(itemList.parentNode.parentNode.parentNode);
 
 // parentElement
-// console.log(itemList.parentElement);
-//  itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement);
+ itemList.parentElement.style.backgroundColor = '#f4f4f4';
 
 // childNodes
-// console.log(itemList.childNodes);
+console.log(itemList.childNodes);
 
-// console.log(itemList.children);
-// console.log(itemList.children[1]);
-// itemList.children[1].style.backgroundColor = 'yellow';
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
 
 // // FirstChild
-// "child" includes white space, always use children
-// console.log(itemList.firstChild);
-// //firstElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent = 'Hello 4';
+"child" includes white space, always use children
+console.log(itemList.firstChild);
+//firstElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello 4';
 
 // nextSibling
-// console.log(itemList.nextSibling);
-// nextElementSibling
-// console.log(itemList.nextElementSibling);
+console.log(itemList.nextSibling);
+nextElementSibling
+console.log(itemList.nextElementSibling);
 
 // previousSibling
-// console.log(itemList.previousSibling);
-// previousElementSibling
-// console.log(itemList.previousElementSibling);
-// itemList.previousElementSibling.style.color = 'green'
+console.log(itemList.previousSibling);
+previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = 'green'
 
 // createElement
 
